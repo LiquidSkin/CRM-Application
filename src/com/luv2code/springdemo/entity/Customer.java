40dfbf450f3e02eeeda9f795709adc5849e7a034
VigneshRaghav <vraghav@udel.edu>
2020-10-07 21:07:50 -0400

    Added CSS and JSP View

--- src/com/luv2code/springdemo/entity/Customer.java
+++ src/com/luv2code/springdemo/entity/Customer.java
@@ -2,34 +2,30 @@ package com.luv2code.springdemo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
 
 @Entity
-@Table(name = "customer")
+@Table(name="customer")
 public class Customer {
-	
+
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	@Column(name = "id")
+	@Column(name="id")
 	private int id;
 	
-	
-	@Column(name = "firstName")
+	@Column(name="first_name")
 	private String firstName;
 	
-	
-	@Column(name = "lastName")
+	@Column(name="last_name")
 	private String lastName;
 	
-	
-	@Column(name = "email")
+	@Column(name="email")
 	private String email;
 	
-	public Customer()
-	{
+	public Customer() {
 		
 	}
 
@@ -67,9 +63,18 @@ public class Customer {
 
 	@Override
 	public String toString() {
-		return "Customer [id=" + id + ", firstName=" + firstName
-				+ ", lastName=" + lastName + ", email=" + email + "]";
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
-	
-
+		
 }
+
+
+
+
+
+
+
+
+
+
+
